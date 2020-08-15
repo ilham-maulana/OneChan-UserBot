6bdb5464b7c40d4e4f837cc5cdcd0be884db7f8b
Ilham_Maulana <61121214+Ilham94@users.noreply.github.com>
2020-08-15 15:48:22 +0700

    Add files via upload

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -7,66 +7,59 @@
 """ Userbot module for other small commands. """
 
 from random import randint
-from asyncio import sleep
+from time import sleep
 from os import execl
 import sys
-import os
 import io
-import sys
-import json
 from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, bot
 from userbot.events import register
+from userbot.utils import time_formatter
 
 
-@register(outgoing=True, pattern="^.random")
+@register(outgoing=True, pattern="^\.random")
 async def randomise(items):
     """ For .random command, get a random item from the list of items. """
     itemo = (items.text[8:]).split()
     if len(itemo) < 2:
-        await items.edit(
+        return await items.edit(
             "`2 or more items are required! Check .help random for more info.`"
         )
-        return
     index = randint(1, len(itemo) - 1)
     await items.edit("**Query: **\n`" + items.text[8:] + "`\n**Output: **\n`" +
                      itemo[index] + "`")
 
 
-@register(outgoing=True, pattern="^.sleep( [0-9]+)?$")
+@register(outgoing=True, pattern="^\.sleep ([0-9]+)$")
 async def sleepybot(time):
     """ For .sleep command, let the userbot snooze for a few second. """
-    message = time.text
-    if " " not in time.pattern_match.group(1):
-        await time.reply("Syntax: `.sleep [seconds]`")
-    else:
-        counter = int(time.pattern_match.group(1))
-        await time.edit("`I am sulking and snoozing....`")
-        await sleep(2)
-        if BOTLOG:
-            await time.client.send_message(
-                BOTLOG_CHATID,
-                "You put the bot to sleep for " + str(counter) + " seconds",
-            )
-        await sleep(counter)
-        await time.edit("`OK, I'm awake now.`")
+    counter = int(time.pattern_match.group(1))
+    await time.edit("`I am sulking and snoozing...`")
+    if BOTLOG:
+        str_counter = time_formatter(counter)
+        await time.client.send_message(
+            BOTLOG_CHATID,
+            f"You put the bot to sleep for {str_counter}.",
+        )
+    sleep(counter)
+    await time.edit("`OK, I'm awake now.`")
 
 
-@register(outgoing=True, pattern="^.shutdown$")
-async def killdabot(event):
+@register(outgoing=True, pattern="^\.shutdown$")
+async def killthebot(event):
     """ For .shutdown command, shut the bot down."""
-    await event.edit("AkameBot Telah Di Nonaktifkan")
+    await event.edit("`Goodbye...`")
     if BOTLOG:
         await event.client.send_message(BOTLOG_CHATID, "#SHUTDOWN \n"
                                         "Bot shut down")
     await bot.disconnect()
 
 
-@register(outgoing=True, pattern="^.restart$")
+@register(outgoing=True, pattern="^\.restart$")
 async def killdabot(event):
     await event.edit("`*i would be back in a moment*`")
     if BOTLOG:
         await event.client.send_message(BOTLOG_CHATID, "#RESTART \n"
-                                        "Bot Telah Direstart")
+                                        "Bot Restarted")
     await bot.disconnect()
     # Spin a new instance of bot
     execl(sys.executable, sys.executable, *sys.argv)
@@ -74,43 +67,20 @@ async def killdabot(event):
     exit()
 
 
-@register(outgoing=True, pattern="^.community$")
-async def bot_community(community):
-    """ For .community command, just returns OG Paperplane's group link. """
-    await community.edit(
-        "Join Ke Channel Saya @NotFakeSquadChannel"
-        "\nButuh Akun Untuk Ngecheat? \nJoin @NFS_AkunStore "
-        "\nNot Fake Squad")
-
-
-@register(outgoing=True, pattern="^.support$")
-async def bot_support(wannahelp):
-    """ For .support command, just returns the group link. """
-    await wannahelp.edit(
-        "Join Ke Channelku: @NotFakeSquadChannel \
-        \nJoin Ke Grup Chat: @NotFakeSquadGrup")
-
-
-@register(outgoing=True, pattern="^.creator$")
-async def creator(e):
-    await e.edit("Creator: [NFS乛Rizky戈手](https://t.me/AkameNFS)")
-
-
-@register(outgoing=True, pattern="^.readme$")
+@register(outgoing=True, pattern="^\.readme$")
 async def reedme(e):
     await e.edit(
         "Here's something for you to read:\n"
-        "\n[OpenUserBot's README.md file](https://github.com/RizkyNFS/AkameBot/blob/sql-extended/README.md)"
+        "\n[OneChan's README.md file](https://github.com/Ilham94/OneChan/blob/master/README.md)"
         "\n[Setup Guide - Basic](https://telegra.ph/How-to-host-a-Telegram-Userbot-11-02)"
-        "\n[Setup Guide - Google Drive](https://telegra.ph/How-To-Setup-GDrive-11-02)"
+        "\n[Setup Guide - Google Drive](https://telegra.ph/How-To-Setup-Google-Drive-04-03)"
         "\n[Setup Guide - LastFM Module](https://telegra.ph/How-to-set-up-LastFM-module-for-Paperplane-userbot-11-02)"
         "\n[Video Tutorial - 576p](https://mega.nz/#!ErwCESbJ!1ZvYAKdTEfb6y1FnqqiLhHH9vZg4UB2QZNYL9fbQ9vs)"
         "\n[Video Tutorial - 1080p](https://mega.nz/#!x3JVhYwR!u7Uj0nvD8_CyyARrdKrFqlZEBFTnSVEiqts36HBMr-o)"
         "\n[Special - Note](https://telegra.ph/Special-Note-11-02)")
-    
 
-# Copyright (c) Gegham Zakaryan | 2019
-@register(outgoing=True, pattern="^.repeat (.*)")
+
+@register(outgoing=True, pattern="^\.repeat (.*)")
 async def repeat(rep):
     cnt, txt = rep.pattern_match.group(1).split(' ', 1)
     replyCount = int(cnt)
@@ -124,15 +94,13 @@ async def repeat(rep):
     await rep.edit(replyText)
 
 
-@register(outgoing=True, pattern="^.repo$")
+@register(outgoing=True, pattern=r"^\.repo$")
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
-    await wannasee.edit(
-        "Klik [disini](https://github.com/RizkyNFS/AkameBot) Untuk Membuka OpenUserBot Halaman GitHub."
-    )
+    await wannasee.edit("[Repo](https://github.com/Ilham94/OneChan) GitHub's page.")
 
 
-@register(outgoing=True, pattern="^.raw$")
+@register(outgoing=True, pattern="^\.raw$")
 async def raw(event):
     the_real_message = None
     reply_to_id = None
@@ -157,61 +125,28 @@ async def raw(event):
 
 
 CMD_HELP.update({
-    'random':
-    '.random <item1> <item2> ... <itemN>\
-\nUsage: Get a random item from the list of items.'
-})
-
-CMD_HELP.update({
-    'sleep':
-    '.sleep <seconds>\
-\nUsage: Userbots get tired too. Let yours snooze for a few seconds.'
-})
-
-CMD_HELP.update({
+    "random":
+    ">`.random <item1> <item2> ... <itemN>`"
+    "\nUsage: Get a random item from the list of items.",
+    "sleep":
+    ">`.sleep <seconds>`"
+    "\nUsage: Let yours snooze for a few seconds.",
     "shutdown":
-    ".shutdown\
-\nUsage: Sometimes you need to shut down your bot. Sometimes you just hope to\
-hear Windows XP shutdown sound... but you don't."
-})
-
-CMD_HELP.update(
-    {'support': ".support\
-\nUsage: If you need help, use this command."})
-
-CMD_HELP.update({
-    'community':
-    ".community\
-\nUsage: Join the awesome Paperplane userbot community !!"
-})
-
-CMD_HELP.update({
-    'repo':
-    '.repo\
-\nUsage: If you are curious what makes the userbot work, this is what you need.'
-})
-
-CMD_HELP.update({
+    ">`.shutdown`"
+    "\nUsage: Shutdown bot",
+    "repo":
+    ">`.repo`"
+    "\nUsage: Github Repo of this bot",
     "readme":
-    ".readme\
-\nUsage: Provide links to setup the userbot and it's modules."
-})
-
-CMD_HELP.update(
-    {"creator": ".creator\
-\nUsage: Know who created this awesome userbot !!"})
-
-CMD_HELP.update({
+    ">`.readme`"
+    "\nUsage: Provide links to setup the userbot and it's modules.",
     "repeat":
-    ".repeat <no.> <text>\
-\nUsage: Repeats the text for a number of times. Don't confuse this with spam tho."
-})
-
-CMD_HELP.update({"restart": ".restart\
-\nUsage: Restarts the bot !!"})
-
-CMD_HELP.update({
+    ">`.repeat <no> <text>`"
+    "\nUsage: Repeats the text for a number of times. Don't confuse this with spam tho.",
+    "restart":
+    ">`.restart`"
+    "\nUsage: Restarts the bot !!",
     "raw":
-    ".raw\
-\nUsage: Get detailed JSON-like formatted data about replied message."
+    ">`.raw`"
+    "\nUsage: Get detailed JSON-like formatted data about replied message."
 })
