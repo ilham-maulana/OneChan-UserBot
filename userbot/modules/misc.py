93ca6110b119c08a1a47ed270a910e47fa5d1c18
Ilham_Maulana <61121214+Ilham94@users.noreply.github.com>
2020-08-16 03:03:39 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -101,7 +101,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "[Repo](https:t.me/ilham_maulana1)"
+        "[Repo](https:t.me/ilham_maulana1) nih"
     )
 
 
