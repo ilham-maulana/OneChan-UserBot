c41a5eddf522b3c467199832c53a9afb8672abd2
Ilham_Maulana <61121214+Ilham94@users.noreply.github.com>
2020-08-16 01:38:27 +0700

    Add files via upload

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -3,22 +3,234 @@
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
-""" Init file which loads all of the modules """
-from userbot import LOGS
+""" Userbot initialization. """
+
+import os
+
+from sys import version_info
+from logging import basicConfig, getLogger, INFO, DEBUG
+from distutils.util import strtobool as sb
+
+from pylast import LastFMNetwork, md5
+from pySmartDL import SmartDL
+from dotenv import load_dotenv
+from requests import get
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+
+load_dotenv("config.env")
+
+# Bot Logs setup:
+CONSOLE_LOGGER_VERBOSE = sb(os.environ.get(
+    "CONSOLE_LOGGER_VERBOSE") or "False")
+
+if CONSOLE_LOGGER_VERBOSE:
+    basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=DEBUG,
+    )
+else:
+    basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=INFO)
+LOGS = getLogger(__name__)
+
+if version_info[0] < 3 or version_info[1] < 8:
+    LOGS.info(
+        "You MUST have a python version of at least 3.8."
+        "Multiple features depend on this. Bot quitting."
+    )
+    quit(1)
+
+# Check if the config was edited by using the already used variable.
+# Basically, its the 'virginity check' for the config file ;)
+CONFIG_CHECK = (os.environ.get(
+    "___________PLOX_______REMOVE_____THIS_____LINE__________") or None)
+
+if CONFIG_CHECK:
+    LOGS.info(
+        "Please remove the line mentioned in the first hashtag from the config.env file"
+    )
+    quit(1)
+
+# Telegram App KEY and HASH
+API_KEY = os.environ.get("API_KEY") or None
+API_HASH = os.environ.get("API_HASH") or None
 
 
-def __list_all_modules():
-    from os.path import dirname, basename, isfile
-    import glob
+# Userbot Session String
+STRING_SESSION = os.environ.get("STRING_SESSION") or None
 
-    mod_paths = glob.glob(dirname(__file__) + "/*.py")
-    all_modules = [
-        basename(f)[:-3] for f in mod_paths
-        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
-    ]
-    return all_modules
+# Deezloader
+DEEZER_ARL_TOKEN = os.environ.get("DEEZER_ARL_TOKEN") or None
+
+# Logging channel/group ID configuration.
+BOTLOG_CHATID = int(os.environ.get("BOTLOG_CHATID") or 0)
+
+# Userbot logging feature switch.
+BOTLOG = sb(os.environ.get("BOTLOG") or "False")
+if BOTLOG:
+    LOGSPAMMER = sb(os.environ.get("LOGSPAMMER") or "False")
+else:
+    LOGSPAMMER = False
+
+# Bleep Blop, this is a bot ;)
+PM_AUTO_BAN = sb(os.environ.get("PM_AUTO_BAN") or "False")
+
+# Heroku Credentials for updater.
+HEROKU_MEMEZ = sb(os.environ.get("HEROKU_MEMEZ") or "False")
+HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME") or None
+HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY") or None
+
+# Github Credentials for updater and Gitupload.
+GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME") or None
+GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN") or None
+
+# Custom (forked) repo URL and BRANCH for updater.
+UPSTREAM_REPO_URL = (os.environ.get("UPSTREAM_REPO_URL")
+                     or "https://github.com/RizkyAkame/ShinoaHiiragi.git")
+UPSTREAM_REPO_BRANCH = os.environ.get("UPSTREAM_REPO_BRANCH") or "sql-extended"
+
+# Console verbose logging
+CONSOLE_LOGGER_VERBOSE = sb(os.environ.get(
+    "CONSOLE_LOGGER_VERBOSE") or "False")
+
+# SQL Database URI
+DB_URI = os.environ.get("DATABASE_URL") or None
+
+# OCR API key
+OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY") or None
+
+# remove.bg API key
+REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY") or None
+
+# Chrome Driver and Headless Google Chrome Binaries
+CHROME_DRIVER = os.environ.get("CHROME_DRIVER") or None
+GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN") or None
+
+# OpenWeatherMap API Key
+OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID") or None
+WEATHER_DEFCITY = os.environ.get("WEATHER_DEFCITY") or None
+WEATHER_DEFLANG = os.environ.get("WEATHER_DEFLANG") or None
+
+# Genius lyrics API
+GENIUS = os.environ.get("GENIUS_ACCESS_TOKEN") or None
+
+# Quotes API Token
+QUOTES_API_TOKEN = os.environ.get("QUOTES_API_TOKEN") or None
+
+# Wolfram Alpha API
+WOLFRAM_ID = os.environ.get("WOLFRAM_ID") or None
+
+# Anti Spambot Config
+ANTI_SPAMBOT = sb(os.environ.get("ANTI_SPAMBOT") or "False")
+ANTI_SPAMBOT_SHOUT = sb(os.environ.get("ANTI_SPAMBOT_SHOUT") or "False")
+
+# Default .alive name
+ALIVE_NAME = os.environ.get("ALIVE_NAME") or None
+
+# Default .alive logo
+ALIVE_LOGO = os.environ.get("ALIVE_LOGO") or None
+
+# Time & Date - Country and Time Zone
+COUNTRY = str(os.environ.get("COUNTRY") or "")
+TZ_NUMBER = int(os.environ.get("TZ_NUMBER") or 1)
+
+# Version of One4uBot
+USERBOT_VERSION = os.environ.get("USERBOT_VERSION") or "3.5"
+
+# User Terminal alias
+USER_TERM_ALIAS = os.environ.get("USER_TERM_ALIAS") or "One4uBot"
+
+# Updater alias
+UPDATER_ALIAS = os.environ.get("UPDATER_ALIAS") or "One4uBot"
+
+# Zipfile module
+ZIP_DOWNLOAD_DIRECTORY = os.environ.get("ZIP_DOWNLOAD_DIRECTORY") or "./zips"
+
+# Clean Welcome
+CLEAN_WELCOME = sb(os.environ.get("CLEAN_WELCOME") or "True")
+
+# Last.fm Module
+BIO_PREFIX = os.environ.get("BIO_PREFIX") or None
+DEFAULT_BIO = os.environ.get("DEFAULT_BIO") or None
+
+LASTFM_API = os.environ.get("LASTFM_API") or None
+LASTFM_SECRET = os.environ.get("LASTFM_SECRET") or None
+LASTFM_USERNAME = os.environ.get("LASTFM_USERNAME") or None
+LASTFM_PASSWORD_PLAIN = os.environ.get("LASTFM_PASSWORD") or None
+LASTFM_PASS = md5(LASTFM_PASSWORD_PLAIN)
+if LASTFM_API is not None:
+    lastfm = LastFMNetwork(
+        api_key=LASTFM_API,
+        api_secret=LASTFM_SECRET,
+        username=LASTFM_USERNAME,
+        password_hash=LASTFM_PASS,
+    )
+else:
+    lastfm = None
+
+# Google Drive Module
+G_DRIVE_DATA = os.environ.get("G_DRIVE_DATA") or None
+G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID") or None
+G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET") or None
+G_DRIVE_AUTH_TOKEN_DATA = os.environ.get("G_DRIVE_AUTH_TOKEN_DATA") or None
+G_DRIVE_FOLDER_ID = os.environ.get("G_DRIVE_FOLDER_ID") or None
+TEMP_DOWNLOAD_DIRECTORY = os.environ.get(
+    "TMP_DOWNLOAD_DIRECTORY") or "./downloads"
+
+# Setting Up CloudMail.ru and MEGA.nz extractor binaries,
+# and giving them correct perms to work properly.
+if not os.path.exists("bin"):
+    os.mkdir("bin")
+
+binaries = {
+    "https://raw.githubusercontent.com/adekmaulana/megadown/master/megadown": "bin/megadown",
+    "https://raw.githubusercontent.com/yshalsager/cmrudl.py/master/cmrudl.py": "bin/cmrudl",
+}
+
+for binary, path in binaries.items():
+    downloader = SmartDL(binary, path, progress_bar=False)
+    downloader.start()
+    os.chmod(path, 0o755)
+
+# 'bot' variable
+if STRING_SESSION:
+    # pylint: disable=invalid-name
+    bot = TelegramClient(StringSession(STRING_SESSION), API_KEY, API_HASH)
+else:
+    # pylint: disable=invalid-name
+    bot = TelegramClient("userbot", API_KEY, API_HASH)
 
 
-ALL_MODULES = sorted(__list_all_modules())
-LOGS.info("Modules to load: %s", str(ALL_MODULES))
-__all__ = ALL_MODULES + ["ALL_MODULES"]
+async def check_botlog_chatid():
+    if not BOTLOG:
+        return
+
+    entity = await bot.get_entity(BOTLOG_CHATID)
+    if entity.default_banned_rights.send_messages:
+        LOGS.info(
+            "Your account doesn't have rights to send messages to BOTLOG_CHATID "
+            "group. Check if you typed the Chat ID correctly.")
+        quit(1)
+
+
+with bot:
+    try:
+        bot.loop.run_until_complete(check_botlog_chatid())
+    except BaseException:
+        LOGS.info(
+            "BOTLOG_CHATID environment variable isn't a "
+            "valid entity. Check your environment variables/config.env file."
+        )
+        quit(1)
+
+# Global Variables
+COUNT_MSG = 0
+USERS = {}
+COUNT_PM = {}
+LASTMSG = {}
+CMD_HELP = {}
+ZALG_LIST = {}
+ISAFK = False
+AFKREASON = None
