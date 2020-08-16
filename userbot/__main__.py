4894ba90e3cd57565b392c7927a7481b95742498
Ilham_Maulana <61121214+Ilham94@users.noreply.github.com>
2020-08-16 16:51:59 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -26,11 +26,11 @@ except PhoneNumberInvalidError:
 for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
-LOGS.info("You are running ShinoaHiiragi")
+LOGS.info("You are running OneChan-UserBot")
 
 LOGS.info(
-    "Congratulations, your userbot is now running !! Test it by typing .alive / .on in any chat."
-    "If you need assistance, head to https://t.me/rizkynfs")
+    "Congratulations, OneChan-UserBot is now running !! Test it by typing .alive / .on in any chat."
+    "If you need assistance, head to https://t.me/ilham_maulana1")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
