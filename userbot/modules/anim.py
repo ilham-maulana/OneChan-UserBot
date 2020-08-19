7550a4409248c97bdc68419375174fd2c48a7ea9
Ilham_Maulana <61121214+Ilham94@users.noreply.github.com>
2020-08-19 17:10:59 +0700

    Create anim.py

---
+++ userbot/modules/anim.py
@@ -0,0 +1,54 @@
+import asyncio
+import re
+import time
+from time import sleep
+from userbot import CMD_HELP, ZALG_LIST
+from userbot.events import register
+
+@borg.on(admin_cmd(pattern="loveu", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 0.5
+    animation_ttl = range(0, 70)
+    await event.edit("loveu")
+    animation_chars = [
+            "😀",
+            "👩‍🎨",
+            "😁",    
+            "😂",
+            "🤣",
+            "😃",
+            "😄",
+            "😅",
+            "😊",
+            "☺",
+            "🙂",    
+            "🤔",
+            "🤨",
+            "😐",
+            "😑",
+            "😶",
+            "😣",
+            "😥",
+            "😮",    
+            "🤐",
+            "😯",
+            "😴",
+            "😔",
+            "😕",
+            "☹",
+            "🙁",
+            "😖",    
+            "😞",
+            "😟",
+            "😢",
+            "😭",
+            "🤯",
+            "💔",
+            "❤",
+            "i Love You❤",   
+        ]
+    for i in animation_ttl:
+            await asyncio.sleep(animation_interval)
+            await event.edit(animation_chars[i % 35])
