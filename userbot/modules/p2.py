a23cb9f8ae3b95aa2fd4536e289b9ec23d543efe
Ilham_Maulana <61121214+Ilham94@users.noreply.github.com>
2020-08-20 12:03:11 +0700

    Update p2

--- userbot/modules/p2.py
+++ userbot/modules/p2.py
@@ -2,11 +2,10 @@ from time import sleep
 from userbot.events import register
 
 
-@register(outgoing=True, pattern='^p(?: |$)(.*)')
+@register(outgoing=True, pattern='^L(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(1)
-    await typew.edit("Biasakanlah Mengucapkan Salam")
+    await typew.edit("Nah Gitu ")
     sleep(1)
-    await typew.edit("Assalamualaikum")
-# Create by myself @AkameNFS
+    await typew.edit(Waalaikumsalam")
