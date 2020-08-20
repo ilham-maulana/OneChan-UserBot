cb78afe92d9d138555c6e65dc593e26aceaf8d97
Ilham_Maulana <61121214+Ilham94@users.noreply.github.com>
2020-08-21 01:36:16 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,54 +1,16 @@
 # Copyright (C) 2019 The Raphielscape Company LLC.
 #
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
 #
 """ Userbot start point """
 
 from importlib import import_module
 from sys import argv
+
 from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
 from userbot import LOGS, bot
 from userbot.modules import ALL_MODULES
-from telethon import TelegramClient
-from userbot.utils import load_module
-from pathlib import Path
-import telethon.utils
-import glob
-
-
-async def add_bot(bot_token):
-    await bot.start(bot_token)
-    bot.me = await bot.get_me()
-    bot.uid = telethon.utils.get_peer_id(bot.me)
-
-
-if len(argv) not in (1, 3, 4):
-    bot.disconnect()
-else:
-    bot.tgbot = None
-    if Var.TG_BOT_USER_NAME_BF_HER is not None:
-        LOGS.info("Initiating Inline Bot")
-        # ForTheGreatrerGood of beautification
-        bot.tgbot = TelegramClient(
-            "TG_BOT_TOKEN",
-            api_id=Var.APP_ID,
-            api_hash=Var.API_HASH
-        ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
-        LOGS.info("Initialisation finished with no errors")
-        LOGS.info("Starting Userbot")
-        bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
-        LOGS.info("Startup Completed")
-    else:
-        bot.start()
-
-path = 'userbot/plugins/*.py'
-files = glob.glob(path)
-for name in files:
-    with open(name) as f:
-        path1 = Path(f.name)
-        shortname = path1.stem
-        load_module(shortname.replace(".py", ""))
 
 
 INVALID_PH = '\nERROR: The Phone No. entered is INVALID' \
@@ -65,10 +27,6 @@ for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
 LOGS.info("You are running OneChan-UserBot")
-
-LOGS.info(
-    "Congratulations, OneChan-UserBot is now running !! Test it by typing .alive / .on in any chat."
-    "If you need assistance, head to t.me/ilham_Maulana1")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
