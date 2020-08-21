444662d4b8960a0168cfd5a540deb86faa1d1bfe
ilham-maulana <70005651+ilham-maulana@users.noreply.github.com>
2020-08-22 03:55:50 +0700

    B

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -197,16 +197,15 @@ async def pipcheck(pip):
 @register(outgoing=True, pattern=r"^.(alive|on)$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    output = (
-        "**▬▬▬▬▬ ❴✪❵ SYSTEM ❴✪❵ ▬▬▬▬▬**\n\n"
-        f"**> Telethon:** {version.__version__}\n"
-        f"**> Python:** {versions.__python_version__}\n"
-        f"**> Firmware:** 1.21.0\n"
-        f"**> Licenza:** 13.4.0\n"
-        f"**> Copyright:**  i̞̟̫̺ͭ̒ͭͣlͬ̍ͤ͆ ͕͖͉̭̰ͨh͚̖̜̍̃͐a̘̫͈̭͌͛͌̇̇̍m̘͈̺̪͓ͩ͂̾ͪ̀̋ Ma̘̫͈̭͌͛͌̇̇̍u̟͎̲͕̼ͮͫͭ̋ͭ͛ͣ̈lͬ̍ͤa̘̫͈̭͌͛͌̇̇̍n̋̋̔ͧa͌͛\n"
-        "**> [UPDATE](https://t.me/ilham_maulana1)\n**"
-        f"**> 👤 USER**: {DEFAULTUSER}\n\n"
-        "▬▬▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬▬▬")
+    output = ("`▬▬▬▬▬❴✪❵SYSTEM❴✪❵▬▬▬▬▬\n"
+              f"> Telethon: {version.__version__}\n"
+              f"> Python: {versions.__python_version__}\n"
+              f"> Firmware: 1.20.0\n"
+              f"> Licenza: 1.32.0}\n"
+              f"> Copyright:  i̞̟̫̺ͭ̒ͭͣlͬ̍ͤ͆h͚̖̜̍̃͐a̘̫͈̭͌͛͌̇̇̍m̘͈̺̪͓ͩ͂̾ͪ̀̋ Ma̘̫͈̭͌͛͌̇̇̍u̟͎̲͕̼ͮͫͭ̋ͭ͛ͣ̈lͬ̍ͤa̘̫͈̭͌͛͌̇̇̍n̋̋̔ͧa͌͛\n"
+              f"> Creator: [ilham](https://t.me/ilham_Maulana1\n"
+              f"> 👤USER: {DEFAULTUSER}\n\n"
+              "`▬▬▬▬▬▬▬▬❴✪❵▬▬▬▬▬▬▬▬`")
     if ALIVE_LOGO:
         logo = ALIVE_LOGO
         await bot.send_file(alive.chat_id, logo, caption=output)
