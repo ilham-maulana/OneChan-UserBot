f6ce328e20839ecdbc77e688e428ad61bb201bf9
ilham-maulana <70005651+ilham-maulana@users.noreply.github.com>
2020-08-22 02:42:22 +0700

    P

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -201,9 +201,9 @@ async def amireallyalive(alive):
         "**▬▬▬▬▬ ❴✪❵ SYSTEM ❴✪❵ ▬▬▬▬▬**\n\n"
         f"**> Telethon:** {version.__version__}\n"
         f"**> Python:** {versions.__python_version__}\n"
-        f"**> Firmware:** {versions.__version__}\n"
-        f"**> Licenza:** {versions.__license__}\n"
-        f"**> Copyright:** {versions.__copyright__}\n"
+        f"**> Firmware:** 1.39.0\n"
+        f"**> Licenza:** 1.22.0\n"
+        f"**> Copyright:**  i̞̟̫̺ͭ̒ͭͣlͬ̍ͤ͆ ͕͖͉̭̰ͨh͚̖̜̍̃͐a̘̫͈̭͌͛͌̇̇̍m̘͈̺̪͓ͩ͂̾ͪ̀̋ Ma̘̫͈̭͌͛͌̇̇̍u̟͎̲͕̼ͮͫͭ̋ͭ͛ͣ̈lͬ̍ͤa̘̫͈̭͌͛͌̇̇̍n̋̋̔ͧa͌͛\n"
         f"**> Creator: [ i̞̟̫̺ͭ̒ͭͣlͬ̍ͤ͆h͚̖̜̍̃͐a̘̫͈̭͌͛͌̇̇̍m̘͈̺̪͓ͩ͂̾ͪ̀̋ Ma̘̫͈̭͌͛͌̇̇̍u̟͎̲͕̼ͮͫͭ̋ͭ͛ͣ̈lͬ̍ͤa̘̫͈̭͌͛͌̇̇̍n̋̋̔ͧa͌͛](https://t.me/ilham_Maulana1\n**"
         f"**> 👤 USER**: {DEFAULTUSER}\n\n"
         "▬▬▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬▬▬")
