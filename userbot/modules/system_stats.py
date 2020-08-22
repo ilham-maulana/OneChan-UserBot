b567e70e1c2f35e7519b8c98246465bc2ce4dc7e
ilham-maulana <70005651+ilham-maulana@users.noreply.github.com>
2020-08-22 23:08:41 +0700

    G

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -213,14 +213,13 @@ async def pipcheck(pip):
 @register(outgoing=True, pattern=r"^.(alive|on)$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    output = (
-        "Hi masTer Bot Telah Aktif.......\n"
+    output = ( "Hi masTer Bot Telah Aktif.......\n"
         f"**▬▬▬▬▬ ❴✪❵ SYSTEM ❴✪❵ ▬▬▬▬▬**\n"
-        f"> 👤 USER: {DEFAULTUSER}\n"
-        f"> 🐍 `Python   : {python_version()} `\n"
-        f"> ⚙️ `Telethon : {version.__version__} `\n"
-        f"> 💻 `Running  : {repo.active_branch.name} `\n"
-        f"> 🗃 `Modules  : {len(modules)} `\n"
+        f"┣[👤 USER: {DEFAULTUSER}\n"
+        f"┣[🐍 `Python   : {python_version()} `\n"
+        f"┣[⚙️ `Telethon : {version.__version__} `\n"
+        f"┣[ 💻 `Running  : {repo.active_branch.name} `\n"
+        f"┣[🗃 `Modules  : {len(modules)} `\n"
         "▬▬▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬▬▬")
 
     if ALIVE_LOGO:
